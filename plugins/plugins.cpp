d1f0f9de49fa211ca958de545158e5942efb2d10
Bloodbat <5965348+Bloodbat@users.noreply.github.com>
2025-11-15 14:24:19 -0600

    Add: new attempt to split modules using preprocessor.

--- plugins/plugins.cpp
+++ plugins/plugins.cpp
@@ -851,6 +851,16 @@ void addThemeMenuItems(Menu*, ModuleTheme*) {}
 #undef modelWerner
 #undef tanh_pade
 
+// Sanguine Monsters
+#define CARDINALMONSTERS
+#include "SanguineMonsters/src/plugin.hpp"
+#undef CARDINALMONSTERS
+
+// Sanguine Mutants
+#define CARDINALMUTANTS
+#include "SanguineMutants/src/plugin.hpp"
+#undef CARDINALMUTANTS
+
 // Sapphire
 #include "Sapphire/src/plugin.hpp"
 
@@ -1012,6 +1022,8 @@ Plugin* pluginInstance__rackwindows;
 Plugin* pluginInstance__RCM;
 Plugin* pluginInstance__RebelTech;
 Plugin* pluginInstance__repelzen;
+Plugin* pluginInstance__SanguineMonsters;
+Plugin* pluginInstance__SanguineMutants;
 Plugin* pluginInstance__sapphire;
 Plugin* pluginInstance__sonusmodular;
 Plugin* pluginInstance__StarlingVia;
@@ -3205,6 +3217,77 @@ static void initStatic__repelzen()
     }
 }
 
+static void initStatic__SanguineMonsters()
+{
+    Plugin* const p = new Plugin;
+    pluginInstance__SanguineMonsters = p;
+
+    const StaticPluginLoader spl(p, "SanguineMonsters");
+    if (spl.ok())
+    {
+        p->addModel(modelSuperSwitch81);
+        p->addModel(modelSuperSwitch18);
+        p->addModel(modelMonstersBlank);
+        p->addModel(modelDollyX);
+        p->addModel(modelOraculus);
+        p->addModel(modelRaiju);
+        p->addModel(modelSphinx);
+        p->addModel(modelBrainz);
+        p->addModel(modelBukavac);
+        p->addModel(modelDungeon);
+        p->addModel(modelKitsune);
+        p->addModel(modelOubliette);
+        p->addModel(modelMedusa);
+        p->addModel(modelAion);
+        p->addModel(modelWerewolf);
+        p->addModel(modelAlchemist);
+        p->addModel(modelChronos);
+        p->addModel(modelFortuna);
+        p->addModel(modelAlembic);
+        p->addModel(modelDenki);
+        p->addModel(modelManus);
+        p->addModel(modelCrucible);
+
+        getMonstersDefaultTheme();
+    }
+}
+
+static void initStatic__SanguineMutants()
+{
+    Plugin* const p = new Plugin;
+    pluginInstance__SanguineMutants = p;
+
+    const StaticPluginLoader spl(p, "SanguineMutants");
+    if (spl.ok())
+    {
+        p->addModel(modelFunes);
+        p->addModel(modelMutantsBlank);
+        p->addModel(modelApices);
+        p->addModel(modelAleae);
+        p->addModel(modelNodi);
+        p->addModel(modelContextus);
+        p->addModel(modelNebulae);
+        p->addModel(modelEtesia);
+        p->addModel(modelMortuus);
+        p->addModel(modelFluctus);
+        p->addModel(modelIncurvationes);
+        p->addModel(modelDistortiones);
+        p->addModel(modelMutuus);
+        p->addModel(modelExplorator);
+        p->addModel(modelMarmora);
+        p->addModel(modelAnuli);
+        p->addModel(modelVelamina);
+        p->addModel(modelAestus);
+        p->addModel(modelTemulenti);
+        p->addModel(modelVimina);
+        p->addModel(modelScalaria);
+        p->addModel(modelNix);
+        p->addModel(modelAnsa);
+
+        getMutantsDefaultTheme();
+    }
+}
+
 static void initStatic__Sapphire()
 {
     Plugin* const p = new Plugin;
@@ -3752,6 +3835,8 @@ void initStaticPlugins()
     initStatic__RCM();
     initStatic__RebelTech();
     initStatic__repelzen();
+    initStatic__SanguineMonsters();
+    initStatic__SanguineMutants();
     initStatic__Sapphire();
     initStatic__sonusmodular();
     initStatic__StarlingVia();
